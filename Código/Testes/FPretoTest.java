356c22107abe491dcaced785494b263077770e66
Breno <brenorosaalmeida@yahoo.com.br>
2023-10-15 16:32:44 -0300

    Implementado os testes da Classe FPrata, FPreto e ProdutoComAdicionais

---
+++ Código/Testes/FPretoTest.java
@@ -0,0 +1,37 @@
+package Testes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import Business.FPreto;
+import Business.Pedido;
+import Business.Produto;
+
+public class FPretoTest {
+    private FPreto fPreto;
+
+    @BeforeEach
+    public void setUp() {
+        fPreto = new FPreto();
+    }
+
+    @Test
+    public void testCalcularDesconto_FPreto() {
+        Produto produto = new Produto(110.0) {
+            @Override
+            public double precoVenda() {
+                return getPrecoBase();
+            }
+        };
+        Pedido pedido = new Pedido(1, LocalDate.now(), produto);
+        fPreto.calcularDesconto(pedido);
+
+        double expectedDiscount = pedido.valorTotal() * 0.1;
+
+        assertEquals(pedido.getValorPago(), pedido.valorTotal() - expectedDiscount, 0.01);
+    }
+}
---
+++ Código/Testes/ProdutosComAdicionaisTest.java
@@ -0,0 +1,58 @@
+package Testes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Business.OpcoesAdicionais;
+import Business.ProdutosComAdicionais;
+
+public class ProdutosComAdicionaisTest {
+    private ProdutosComAdicionais produtoComAdicionais;
+
+    @BeforeEach
+    public void setUp() {
+        produtoComAdicionais = new ProdutosComAdicionais(10.0) {
+            @Override
+            public double precoVenda() {
+                return getPrecoBase() + precoAdicionais();
+            }
+        };
+    }
+
+    @Test
+    public void testAddAdicional() {
+        OpcoesAdicionais adicional = OpcoesAdicionais.QUEIJO;
+        produtoComAdicionais.addAdicional(adicional);
+
+        assertTrue(produtoComAdicionais.getAdicionais().contains(adicional));
+    }
+
+    @Test
+    public void testPrecoAdicionais() {
+        OpcoesAdicionais queijo = OpcoesAdicionais.QUEIJO;
+        OpcoesAdicionais bacon = OpcoesAdicionais.BACON;
+
+        produtoComAdicionais.addAdicional(queijo);
+        produtoComAdicionais.addAdicional(bacon);
+
+        double precoEsperado = queijo.getPreco() + bacon.getPreco();
+
+        assertEquals(precoEsperado, produtoComAdicionais.precoAdicionais());
+    }
+
+    @Test
+    public void testPrecoVendaComAdicionais() {
+        OpcoesAdicionais queijo = OpcoesAdicionais.QUEIJO;
+        OpcoesAdicionais bacon = OpcoesAdicionais.BACON;
+
+        produtoComAdicionais.addAdicional(queijo);
+        produtoComAdicionais.addAdicional(bacon);
+
+        double precoEsperado = produtoComAdicionais.getPrecoBase() + queijo.getPreco() + bacon.getPreco();
+
+        assertEquals(precoEsperado, produtoComAdicionais.precoVenda());
+    }
+}
